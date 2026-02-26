9bd39c329a3870a553aa9b6c2f11236b1c123c5e
Thomas Sitnik <txsitnik@gmail.com>
2026-02-26 14:22:05 -0500

    Added json.simple

--- src/main/java/com/model/Driver.java
+++ src/main/java/com/model/Driver.java
@@ -1,7 +1,6 @@
 package com.model;
 
 public class Driver {
-    
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.interview {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.interview to javafx.fxml;
     exports com.interview;
