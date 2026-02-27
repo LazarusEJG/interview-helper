ed503a431fdb33bc62139733aacb75f3f18e6d21
Ethan-Russell-CSE <ethanor@email.sc.edu>
2026-02-27 10:56:48 -0500

    added getters to Solution.java

--- src/main/java/com/model/Solution.java
+++ src/main/java/com/model/Solution.java
@@ -1,15 +1,34 @@
 package com.model;
 
+import java.util.UUID;
+
 public class Solution extends Response {
-    private String file;
-    private String explanation;
-    private boolean verified;
+	private UUID id;
+	private String file;
+	private String explanation;
+	private boolean verified;
 
-    public Solution(User author, String filename) {
-        
-    }
+	public Solution(User author, String filename) {
 
-    public Solution(User author, String filename, String explanation) {
-        
-    }
+	}
+
+	public Solution(UUID id, User author, String filename, String explanation) {
+
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public String getExplanation() {
+		return explanation;
+	}
+
+	public boolean isVerified() {
+		return verified;
+	}
 }
