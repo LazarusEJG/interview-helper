ae4930537177de23cbb0aa636b9088836e36ff2f
Ethan-Russell-CSE <ethanor@email.sc.edu>
2026-02-27 09:40:42 -0500

    fixed LocalDate import in user

--- src/main/java/com/model/Question.java
+++ src/main/java/com/model/Question.java
@@ -5,65 +5,65 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Question implements Commentable {
-    private UUID id; //added to constructor because I felt it probably needed to be there
-    private String title;
-    private int difficulty;
-    private int score;
-    private String content;
-    private ArrayList<String> categories;
-    private ArrayList<Solution> solutions;
-    private ArrayList<Comment> comments;
-    private User author;
-    private ArrayList<String> hints;
-    private LocalDateTime publishTime;
+	private UUID id; // added to constructor because I felt it probably needed to be there
+	private String title;
+	private int difficulty;
+	private int score;
+	private String content;
+	private ArrayList<String> categories;
+	private ArrayList<Solution> solutions;
+	private ArrayList<Comment> comments;
+	private User author;
+	private ArrayList<String> hints;
+	private LocalDateTime publishTime;
 
-    public Question (User author, String content) {
-        this.author = author;
-        this.content = content;
-    }
+	public Question(User author, String content) {
+		this.author = author;
+		this.content = content;
+	}
 
-    public Question(UUID id, User author, String content, String title,
-        ArrayList<String> categories, ArrayList<Solution> solutions,
-        ArrayList<Comment> comments, ArrayList<String> hints,
-        LocalDateTime publishTime, int score) {
-            this.id = id; //refer to UUID instance variable
-            this.author = author;
-            this.content = content;
-            this.title = title;
-            this.categories = categories;
-            this.solutions = solutions;
-            this.comments = comments;
-            this.hints = hints;
-            this.publishTime = publishTime;
-            this.score = score;
+	public Question(UUID id, User author, String content, String title,
+			ArrayList<String> categories, ArrayList<Solution> solutions,
+			ArrayList<Comment> comments, ArrayList<String> hints,
+			LocalDateTime publishTime, int score) {
+		this.id = id; // refer to UUID instance variable
+		this.author = author;
+		this.content = content;
+		this.title = title;
+		this.categories = categories;
+		this.solutions = solutions;
+		this.comments = comments;
+		this.hints = hints;
+		this.publishTime = publishTime;
+		this.score = score;
 
-    }
+	}
 
-    public void upVote() {
+	public void upVote() {
 
-    }
+	}
 
-    public void downVote() {
+	public void downVote() {
 
-    }
+	}
 
-    public void addSolution(User user, Solution solution) {
+	public void addSolution(User user, Solution solution) {
 
-    }
+	}
 
-    public void addComment(Comment comment) {
+	public void addComment(Comment comment) {
 
-    }
+	}
 
-    public void removeResponse(User user, Response response) {
-        
-    }
+	public void removeResponse(User user, Response response) {
 
-    public ArrayList<String> getHints() {
-    return hints; //temporary statement & changed to match return types (change if wrong)
-    }
+	}
 
-    public ArrayList<Solution> getSolutions() {
-     return solutions; //temporary statement
-    }
+	public ArrayList<String> getHints() {
+		return hints; // temporary statement & changed to match return types (change if wrong)
+	}
+
+	public ArrayList<Solution> getSolutions() {
+		return solutions; // temporary statement
+	}
 }
