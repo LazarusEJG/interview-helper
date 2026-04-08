87223443e658e8459a0b71260052d21ad40cd37b
LazarusEJG <ej2001e@gmail.com>
2026-04-07 23:54:18 -0400

    finished Home Screen

--- src/main/java/com/interview/App.java
+++ src/main/java/com/interview/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Home"), 1080, 720);
+        scene = new Scene(loadFXML("Home"), 1200, 800);
         stage.setScene(scene);
         stage.show();
     }
