aeecccc7d100d583ecb5bd130054ede734dab545
LazarusEJG <ej2001e@gmail.com>
2026-04-19 01:19:38 -0400

    worked towards #99

--- src/main/java/com/controllers/ProfileController.java
+++ src/main/java/com/controllers/ProfileController.java
@@ -6,11 +6,45 @@ import com.interview.App;
 import com.model.InterviewApp;
 import com.model.User;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+
 
 public class ProfileController {
+
+    @FXML
+    private ComboBox<String> categorySelector;
+
+    @FXML
+    private VBox contentContainer;
+
+    @FXML
+    private Label interestsLabel;
+
+    @FXML 
+    private Label votesLabel;
+
+    @FXML 
+    private Label streakLabel;
+
+    @FXML 
+    private Label longestStreakLabel;
+
+    @FXML 
+    private Label lastStreakLabel;
+
+    @FXML
+    private ImageView avatarImage;
+
+    @FXML
+    private Circle avatarClip;
 
     @FXML
     private Label welcomeLabel;
@@ -18,9 +52,33 @@ public class ProfileController {
     @FXML
     public void initialize() {
         InterviewApp library = App.getInterviewApp();
+        //top rectangle
         User user = library.getCurrentUser();
-        welcomeLabel.setText("Welcome in! " + user.getUsername());
+        welcomeLabel.setText("@" + user.getUsername());
+        interestsLabel.setText(String.valueOf("Interests: "+user.getInterests()));
 
+        //middle image
+        Image img = new Image(
+        getClass().getResourceAsStream("/com/interview/images/avatar-placeholder.jpg"),
+        140, 140,
+        false,
+        true
+        );
+        avatarImage.setImage(img);
+        Platform.runLater(() -> {
+        Circle clip = new Circle(100);
+        clip.setCenterX(100);
+        clip.setCenterY(100);
+        avatarImage.setClip(clip);
+        });
+
+        //bottom recangle
+        votesLabel.setText(String.valueOf(user.getReceivedVotes()));
+        streakLabel.setText(String.valueOf(user.getCurrentStreak()));
+        longestStreakLabel.setText(String.valueOf(user.getLongestStreak()));
+        lastStreakLabel.setText(String.valueOf(user.getLastStreakDate()));
+
+        //right side (start here)
     }
 
     @FXML
